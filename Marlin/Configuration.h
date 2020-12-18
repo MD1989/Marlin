a157192ce877bf7fecfa704bd6734836d0fa35bd
MD1989 <51494910+MD1989@users.noreply.github.com>
2020-12-18 17:51:48 +0100

    Bed Size - Clips
    Altered Bed Size for clips. Adjust Y-stop to clear front clips, then shrink bed size to clear rear clips.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -986,7 +986,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 20
+#define PROBING_MARGIN 5
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (133*60)
@@ -1119,7 +1119,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define Y_BED_SIZE 210
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
